b8623780c58fa49d549832088572910c6c1ef83b
windurisky <windu.risky@bukalapak.com>
2020-10-21 14:04:16 +0700

    check featured image instead of new arrivals

--- lib/sneaker_watcher_bot/services/scrapers/atmos/detect_new_arrivals_change.rb
+++ lib/sneaker_watcher_bot/services/scrapers/atmos/detect_new_arrivals_change.rb
@@ -26,12 +26,12 @@ module Service
         end
 
         def scrape_new_arrival
-          response = RestClient.get("#{@atmos_base_url}/collections/new-arrivals")
+          response = RestClient.get("#{@atmos_base_url}")
           html = Nokogiri::HTML(response.body)
           latest_product_grid = html.css('.product.grid-item').first
           latest_product_url = latest_product_grid.elements.first.attributes['href'].value
           latest_product_name = latest_product_grid.css('.product-title').first.inner_text
-          latest_product_img = latest_product_grid.xpath("//*[@id='panel']/section/div[2]/div[2]/div/div[1]/article/a/div[1]/img").first.attributes['src'].value
+          latest_product_img = latest_product_grid.css('.product-featured-image').first.elements.first.attributes['src'].value
           latest_product_img = "https:#{latest_product_img}"
           {
             name: latest_product_name,
