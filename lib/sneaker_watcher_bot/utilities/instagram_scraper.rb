c897624bdf22271316bffbb9662b9703291f0175
windurisky <windu.risky@bukalapak.com>
2020-10-23 23:34:23 +0700

    close watir browser so that next instagram scraper can be initiated without error

--- lib/sneaker_watcher_bot/utilities/instagram_scraper.rb
+++ lib/sneaker_watcher_bot/utilities/instagram_scraper.rb
@@ -12,33 +12,33 @@ class InstagramScraper
   end
 
   def get_stories(target_username)
-    @cookies ||= begin
-      # login
-      @browser.goto "#{INSTAGRAM_BASE_URL}/accounts/login/?force_classic_login"
-      @browser.text_field(id: 'id_username').set @username
-      @browser.text_field(id: 'id_enc_password').set @password
-      @browser.button(type: 'submit').click
-      
-      # get stories data
-      Watir::Wait.until { @browser.text.include? 'INSTAGRAM FROM FACEBOOK' }
-      @browser.goto "#{INSTAGRAM_BASE_URL}/#{target_username}/?__a=1"
-      Watir::Wait.until { @browser.text.include? 'logging_page_id' }
-      profile = JSON.parse(@browser.text).deep_symbolize_keys
-      reel_id = profile[:logging_page_id].split('_')[1]
-      variables = {
-        reel_ids: [reel_id],
-        tag_names: [],
-        location_ids: [],
-        highlight_reel_ids: [],
-        precomposed_overlay: false,
-        show_story_viewer_list: true,
-        story_viewer_fetch_count: 50,
-        stories_video_dash_manifest: false
-      }
-      @browser.goto "#{INSTAGRAM_BASE_URL}/graphql/query/?query_hash=c9c56db64beb4c9dea2d17740d0259d9&variables=#{variables.to_json}"
-      Watir::Wait.until { @browser.text.include? 'reels_media' }
-      raw_stories_data = JSON.parse(@browser.text).deep_symbolize_keys
-      raw_stories_data.dig(:data, :reels_media)&.first&.dig(:items) || []
-    end
+    # login
+    @browser.goto "#{INSTAGRAM_BASE_URL}/accounts/login/?force_classic_login"
+    @browser.text_field(id: 'id_username').set @username
+    @browser.text_field(id: 'id_enc_password').set @password
+    @browser.button(type: 'submit').click
+    
+    # get stories data
+    Watir::Wait.until { @browser.text.include? 'INSTAGRAM FROM FACEBOOK' }
+    @browser.goto "#{INSTAGRAM_BASE_URL}/#{target_username}/?__a=1"
+    Watir::Wait.until { @browser.text.include? 'logging_page_id' }
+    profile = JSON.parse(@browser.text).deep_symbolize_keys
+    reel_id = profile[:logging_page_id].split('_')[1]
+    variables = {
+      reel_ids: [reel_id],
+      tag_names: [],
+      location_ids: [],
+      highlight_reel_ids: [],
+      precomposed_overlay: false,
+      show_story_viewer_list: true,
+      story_viewer_fetch_count: 50,
+      stories_video_dash_manifest: false
+    }
+    @browser.goto "#{INSTAGRAM_BASE_URL}/graphql/query/?query_hash=c9c56db64beb4c9dea2d17740d0259d9&variables=#{variables.to_json}"
+    Watir::Wait.until { @browser.text.include? 'reels_media' }
+    raw_stories_data = JSON.parse(@browser.text).deep_symbolize_keys
+    stories = raw_stories_data.dig(:data, :reels_media)&.first&.dig(:items) || []
+    @browser.close
+    stories
   end
 end
