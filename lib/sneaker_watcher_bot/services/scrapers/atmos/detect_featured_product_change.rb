0df87d7841cdb62d65cd21db31eb0f987b1821c4
windurisky <windu.risky@bukalapak.com>
2020-10-24 11:51:39 +0700

    differentiate between featured product and new arrivals

--- lib/sneaker_watcher_bot/services/scrapers/atmos/detect_featured_product_change.rb
+++ lib/sneaker_watcher_bot/services/scrapers/atmos/detect_featured_product_change.rb
@@ -1,33 +1,33 @@
 module Service
   module Scraper
     module Atmos
-      class DetectNewArrivalsChange < Base
+      class DetectFeaturedProductChange < Base
         def initialize
           @atmos_base_url = ENV['ATMOS_BASE_URL']
         end
 
         def call
-          new_hash = scrape_new_arrival
+          new_hash = scrape_featured_product
           if is_new_product?(new_hash)
             SneakerWatcherBot.redis.set(redis_key, new_hash.to_json)
-            message = "*ATMOS NEW ARRIVAL UPDATE DETECTED!*\n"\
-            "#{new_hash[:name]}\n"
-            "[CHECK IT OUT!](#{@atmos_base_url}#{new_hash[:url]})"
+            message = "*ATMOS FEATURED PRODUCT UPDATE DETECTED!*\n"\
+              "#{new_hash[:name]}\n"
+              "[CHECK IT OUT!](#{@atmos_base_url}#{new_hash[:url]})"
             TelegramBot.new.send_telegram_photo(message, new_hash[:image])
           end
         end
 
         def is_new_product?(new_hash)
           @is_new_product ||= begin
-            latest_product_new_arrival_cache = SneakerWatcherBot.redis.get(redis_key)
-            return true if latest_product_new_arrival_cache.blank?
-            previous_hash = JSON.parse(latest_product_new_arrival_cache).deep_symbolize_keys
+            latest_product_featured_product_cache = SneakerWatcherBot.redis.get(redis_key)
+            return true if latest_product_featured_product_cache.blank?
+            previous_hash = JSON.parse(latest_product_featured_product_cache).deep_symbolize_keys
             previous_hash != new_hash
           end
         end
 
-        def scrape_new_arrival
-          response = RestClient.get("#{@atmos_base_url}/collections/new-arrivals")
+        def scrape_featured_product
+          response = RestClient.get("#{@atmos_base_url}")
           html = Nokogiri::HTML(response.body)
           latest_product_grid = html.css('.product.grid-item').first
           latest_product_url = latest_product_grid.elements.first.attributes['href'].value
@@ -44,7 +44,7 @@ module Service
         private
 
         def redis_key
-          'atmos_latest_product_new_arrivals'
+          'atmos_latest_product_featured_products'
         end
       end
     end
