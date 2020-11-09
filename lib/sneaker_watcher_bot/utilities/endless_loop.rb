fbff05bddef8b5290c5160a81d0c3b515efe90bd
Windu Risky Adiatma <30619149+windurisky@users.noreply.github.com>
2020-11-09 23:59:51 +0700

    add logging when sleep for a better overview of logging (#24)
    Co-authored-by: windurisky <windu.risky@bukalapak.com>

--- lib/sneaker_watcher_bot/utilities/endless_loop.rb
+++ lib/sneaker_watcher_bot/utilities/endless_loop.rb
@@ -1,12 +1,20 @@
 class EndlessLoop
   class << self
-    def perform(interval_seconds)
+    def perform(interval_seconds, background_name)
       while true do
         start_time = Time.now
         yield
         latency = (Time.now - start_time).round
         sleep_duration = [interval_seconds - latency, 0].max
-        sleep(sleep_duration) if sleep_duration > 0
+        if sleep_duration > 0
+          log_object = {
+            message: "#{background_name} with interval of #{interval_seconds} will re-run in #{sleep_duration} seconds",
+            interval_seconds: interval_seconds,
+            background_name: background_name
+          }
+          SneakerWatcherBot.logger.info(log_object)
+          sleep(sleep_duration)
+        end
       end
     end
   end
