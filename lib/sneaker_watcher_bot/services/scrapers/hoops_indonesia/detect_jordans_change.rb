31beba96592a10c88db5c116b7d2bb0710124293
Windu Risky Adiatma <30619149+windurisky@users.noreply.github.com>
2020-11-11 03:57:25 +0700

    Rework hoops indonesia (#25)
    * fix hoops point
    
    * disable banners for a while
    
    * revert into real condition of sizing
    
    Co-authored-by: windurisky <windu.risky@bukalapak.com>

--- lib/sneaker_watcher_bot/services/scrapers/hoops_indonesia/detect_jordans_change.rb
+++ lib/sneaker_watcher_bot/services/scrapers/hoops_indonesia/detect_jordans_change.rb
@@ -2,58 +2,90 @@ module Service
   module Scraper
     module HoopsIndonesia
       class DetectJordansChange < Base
-        AIR_JORDAN = 'AIR JORDAN'
-
         def perform
-          new_hash = scrape_new_jordan
-          if !new_hash.blank? && is_new_product?(new_hash)
-            message = "<strong>HOOPS INDONESIA JORDANS UPDATE DETECTED!</strong>\n"\
-              "#{new_hash[:name]}\n"\
-              "<a href='#{new_hash[:url]}'>CHECK IT OUT!</a>"
-            TelegramBot.new.send_telegram_photo(message, new_hash[:image])
-            SneakerWatcherBot.redis.set(redis_key, new_hash.to_json)
+          payload = {
+            parent_id: '5', # BRAND
+            sub_id: '13' # JORDAN
+          }
+          response = RestClient.post("#{api_url}/cms/product/v2/category/1/12?sort=id&order=DESC", payload.to_json, {content_type: :json, accept: :json})
+          parsed_body = JSON.parse(response.body).deep_symbolize_keys
+          jordans = parsed_body.dig(:body, :data)
+          jordans.each do |jordan|
+            begin
+              product_id = jordan[:id]
+              product_url = "#{base_url}/detail/?id=#{product_id}"
+              product_name = jordan[:name]
+              product_img = jordan[:images_group].first[:imageUrl]
+              product_hash = {
+                id: product_id,
+                name: product_name,
+                url: product_url,
+                image: product_img
+              }
+              sizes = available_sizes(jordan)
+              next if sizes.blank? || # no size available (sold out)
+                !relevant_product?(product_name) || # not relevant
+                !new_product?(product_id) # not a new product
+              send_message(product_hash, sizes)
+            rescue
+              # TODO: Log the error rescued
+            end
           end
-        end
-
-        def is_new_product?(new_hash)
-          @is_new_product ||= begin
-            latest_cache = SneakerWatcherBot.redis.get(redis_key)
-            return true if latest_cache.blank?
-            previous_hash = JSON.parse(latest_cache).deep_symbolize_keys
-            previous_hash != new_hash
-          end
-        end
-
-        def scrape_new_jordan
-          response = RestClient.get("#{base_url}/15-jordan")
-          html = Nokogiri::HTML(response.body)
-          new_items = html.css('.new-box')
-          new_items.each do |new|
-            latest_product_name = new.parent.xpath(".//a[contains(@class,'product_img_link')]").first.attributes['title'].value
-            next unless contains_keyword?(latest_product_name)
-            latest_product_url = new.parent.xpath(".//a[contains(@class,'product_img_link')]").first.attributes['href'].value
-            latest_product_img = new.parent.xpath(".//img").first.attributes['src'].value
-            return {
-              name: latest_product_name,
-              url: latest_product_url,
-              image: latest_product_img
-            }
-          end
-          {}
         end
 
         private
 
-        def contains_keyword?(name)
-          name.upcase =~ /#{AIR_JORDAN}/
+        def whitelisted_products
+          ['air jordan']
         end
 
-        def redis_key
-          'hoops_indonesia_latest_product_jordans'
+        def available_sizes(jordan)
+          sizes = jordan.dig(:size_quantity) || []
+          sizes.each_with_object([]) do |size, available|
+            next if size[:quantity] == 0
+            available << {
+              value: size[:size],
+              quantity: size[:quantity].to_i
+            }
+          end
+        end
+
+        def new_product?(identifier)
+          key = redis_key(identifier)
+          product_cache = SneakerWatcherBot.redis.get(key)
+          return true if product_cache.blank?
+          SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
+          return false
+        end
+
+        def redis_expiry
+          Time.now + 24.hours
+        end
+
+        def send_message(product_hash, sizes)
+          message = "<strong>HOOPS INDONESIA JORDANS UPDATE DETECTED!</strong>\n"\
+            "#{product_hash[:name]}\n"\
+            "<a href='#{product_hash[:url]}'>CHECK IT OUT!</a>"
+          message += "\n\n<strong>AVAILABLE SIZE</strong>:"
+          # sizes.sort_by! { |s| s[:value]}
+          sizes.each do |size|
+            next if size[:quantity] == 0
+            message += "\n<strong>US #{size[:value]}</strong> -- #{size[:quantity]} PCS"
+          end
+          TelegramBot.new.send_telegram_photo(message, product_hash[:image])
+          SneakerWatcherBot.redis.set(redis_key(product_hash[:id]), product_hash.to_json)
+        end
+
+        def redis_key(identifier)
+          "hoops_indonesia_latest_jordans_#{identifier}"
         end
 
         def base_url
           ENV['HOOPS_INDONESIA_BASE_URL']
+        end
+
+        def api_url
+          ENV['HOOPS_INDONESIA_API_URL']
         end
       end
     end
