34f601f9454dc42c1cf38b99ae9ed372dc000c1d
windurisky <windu.risky@bukalapak.com>
2020-11-29 20:10:05 +0700

    in progress encrypt password for instagram body

--- lib/sneaker_watcher_bot/utilities/private_instagram_api/get_csrf_token.rb
+++ lib/sneaker_watcher_bot/utilities/private_instagram_api/get_csrf_token.rb
@@ -1,15 +1,17 @@
 module PrivateInstagramApi
   class GetCsrfToken
-    def initialize(username, password)
+    def initialize(username, password, device_id, android_id)
       @username = username
       @password = password
+      @device_id = device_id
+      @android_id = android_id
     end
   
     def perform
       begin
         response = RestClient.get("#{base_url}/zr/token/result/", request_headers)
         cookies = response.cookies.deep_symbolize_keys
-        Normalizer::CsrfTokenResponse.new(cookies[:csrftoken], cookies[:mid], device_id, android_id)
+        Normalizer::CsrfTokenResponse.new(cookies[:csrftoken], cookies[:mid])
       rescue => e
         log_object = {
           tags: self.class.name.underscore,
@@ -33,8 +35,8 @@ module PrivateInstagramApi
         'X-IG-App-Locale': 'en_US',
         'X-IG-Device-Locale': 'en_US',
         'X-IG-Mapped-Locale': 'en_US',
-        'X-IG-Device-ID': device_id,
-        'X-IG-Android-ID': android_id,
+        'X-IG-Device-ID': @device_id,
+        'X-IG-Android-ID': @android_id,
         'X-IG-App-ID': '567067343352427',
         'X-FB-HTTP-Engine': 'Liger',
         'X-FB-Client-IP': 'True'
@@ -43,20 +45,11 @@ module PrivateInstagramApi
 
     def query_string
       {
-        device_id: android_id,
+        device_id: @android_id,
         token_hash: '',
-        custom_device_id: device_id,
+        custom_device_id: @device_id,
         fetch_reason: 'token_expired'
       }
-    end
-  
-    def device_id
-      # generate uuid based on username password
-      Digest::UUID.uuid_v5(@username, @password)
-    end
-    
-    def android_id
-      @android_id ||= "android-#{SecureRandom.hex(8)}"
     end
   end
 end
--- lib/sneaker_watcher_bot/utilities/private_instagram_api/get_login_cookies.rb
+++ lib/sneaker_watcher_bot/utilities/private_instagram_api/get_login_cookies.rb
@@ -1,6 +1,3 @@
-require 'watir'
-require 'webdrivers/chromedriver'
-
 module PrivateInstagramApi
   class GetLoginCookies
     def initialize(username, password)
@@ -13,7 +10,13 @@ module PrivateInstagramApi
         key = redis_instagram_cookies_key(@username)
         raw_cookies = SneakerWatcherBot.redis.get(key)
         return JSON.parse(raw_cookies).deep_symbolize_keys if raw_cookies.present?
-        @csrf_token_response = GetCsrfToken.new(@username, @password).perform
+        @public_key_response = GetPublicKey.new(@username, @password).perform
+        @csrf_token_response = GetCsrfToken.new(
+          @username,
+          @password,
+          @public_key_response.device_id,
+          @public_key_response.android_id
+        ).perform
         SneakerWatcherBot.redis.set(key, cookie_hash.to_json)
         SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
         response.cookies
@@ -26,64 +29,6 @@ module PrivateInstagramApi
         }
         SneakerWatcherBot.logger.error(log_object)
         raise
-      end
-    end
-  
-    def get_cookies
-      key = redis_instagram_cookies_key(@username)
-      raw_cookies = SneakerWatcherBot.redis.get(key)
-      return JSON.parse(raw_cookies).deep_symbolize_keys if raw_cookies.present?
-      # cookies expired
-      # do relogin to get latest cookies
-      relogin
-      cookie_hash = parse_cookies_from_watir_browser
-      SneakerWatcherBot.redis.set(key, cookie_hash.to_json)
-      SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
-      cookie_hash
-    end
-  
-    def reel_id(target_username, cookies)
-      key = redis_instagram_reel_id(target_username)
-      reel_id = SneakerWatcherBot.redis.get(key)
-      return reel_id if reel_id.present?
-      url = "#{INSTAGRAM_BASE_URL}/#{target_username}/?__a=1"
-      response = RestClient.get(url, cookies: cookies)
-      profile = JSON.parse(response.body).deep_symbolize_keys
-      reel_id = profile[:logging_page_id].split('_')[1]
-      SneakerWatcherBot.redis.set(key, reel_id)
-      SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
-      reel_id
-    end
-  
-    def set_instagram_account
-      # randomize from a pool of instagram accounts
-      account = ENV['INSTAGRAM_ACCOUNTS'].split(',').map(&:strip).compact.sample
-      @username, @password = account.split(':')
-      log_object = {
-        tags: self.class.name.underscore,
-        message: "Set instagram scraper account",
-        instagram_username: @username
-      }
-      SneakerWatcherBot.logger.info(log_object)
-    end
-  
-    def relogin
-      # do re-login to get cookies
-      return if @logged_in
-      
-      @browser.goto 
-      @browser.text_field(id: 'id_username').set @username
-      @browser.text_field(id: 'id_enc_password').set @password
-      @browser.button(type: 'submit').click
-      # indicator that we're logged in
-      Watir::Wait.until { @browser.title.strip.upcase == 'INSTAGRAM' }
-      @logged_in = true
-    end
-  
-    def parse_cookies_from_watir_browser
-      latest_cookies = @browser.cookies.to_a
-      latest_cookies.each_with_object({}) do |cookie, cookie_hash|
-        cookie_hash[cookie[:name].to_sym] = cookie[:value]
       end
     end
   
@@ -109,17 +54,17 @@ module PrivateInstagramApi
         'X-IG-Device-ID': @csrf_token_response.device_id,
         'X-IG-Android-ID': @csrf_token_response.android_id,
         'X-IG-App-ID': '567067343352427',
-        'X-MID': @csrf_token_response.mid
+        'X-MID': @csrf_token_response.mid,
         'X-FB-HTTP-Engine': 'Liger',
         'X-FB-Client-IP': 'True',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        content_type: :json,
+        accept: :json
       }
     end
 
     def request_body
       {
-        
+        # signed_bodySIGNATURE.%7B%22jazoest%22%3A%2222326%22%2C%22country_codes%22%3A%22%5B%7B%5C%22country_code%5C%22%3A%5C%221%5C%22%2C%5C%22source%5C%22%3A%5B%5C%22default%5C%22%5D%7D%5D%22%2C%22phone_id%22%3A%225a8198e4-aef7-4204-9121-2d8abc9861a2%22%2C%22enc_password%22%3A%22%23PWD_INSTAGRAM%3A4%3A1606412328%3AAbr37cSm%2Bn4s1cLMdbgAATKznHkIMRhJ2a3rVaPt5lIOCGQSFpfiNuFQ8vgNPWjgb20SXhp1P6i%2BVv55LyUFxD3UW9XFOJapy7z5Pb%2F%2BbnAPC9847Yf46WeGIjWS7U20BQDWtUt%2Bxb34XK0JKU8eEpBHacBcg0rmCGfHTVqZDQJc%2FAO3O5iySGMBikQSUYMY1PjwkoVI1S4kkyMoC2ACEQsD2ElzF43%2BMMCw9cOzszRwXsQcG3ltxt1%2FpF3nIbmai96FDjbrLvRCrUrmKH9TPfqMvsVhAfE6BIIRAVKXQ5abA%2FrNEDUxBLuq6Oq%2BKKJnUb6Q5Bxcpda8Bm4b6sZTryPVMUr%2FRTsga%2BpUc%2FHTGlOj1%2FgARLuYIW8kz6zi%2FK%2BGWOynDdiX77%2BPT0Ef%22%2C%22_csrftoken%22%3A%22ZKroefVz4stGdTgm2ORoS9TsFXD4dDXi%22%2C%22username%22%3A%22just.a.watcher.69%22%2C%22adid%22%3A%22%22%2C%22guid%22%3A%22dcd5c6f0-e663-412d-b69c-5d95ad76f0c1%22%2C%22device_id%22%3A%22android-b12551c0f35ad072%22%2C%22google_tokens%22%3A%22%5B%5D%22%2C%22login_attempt_count%22%3A%221%22%7D
       }
     end
 
@@ -128,6 +73,22 @@ module PrivateInstagramApi
         mid: @csrf_token_response.mid,
         csrftoken: @csrf_token_response.csrf_token
       }
+    end
+
+    private
+
+    def get_enc_password
+      decoded_public_key = Base64.decode64(@public_key_response.public_key)
+      cipher_aes = OpenSSL::Cipher.new('AES-256-GCM')
+      cipher_aes.encrypt
+      random_key = cipher_aes.random_key
+      iv = cipher_aes.random_iv
+      time = Time.now.to_i.to_s
+      cipher_rsa = OpenSSL::PKey::RSA.new(decoded_public_key)
+      enc_random_key = cipher_rsa.public_encrypt(random_key)
+      chipered_password = cipher_aes.update(@password.encode('utf-8')) + cipher.final
+      # cipher_aes.update(time.encode)
+      "#PWD_INSTAGRAM:4:#{time}:#{payload}"
     end
   end
 end
