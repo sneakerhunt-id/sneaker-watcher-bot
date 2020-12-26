45ed79f24a41a94e0038f4579b9326ba09eb8064
windurisky <windu.risky@bukalapak.com>
2020-12-27 00:35:02 +0700

    collection detection v2, with size checkout

--- lib/sneaker_watcher_bot/services/scrapers/atmos/detect_collections_change.rb
+++ lib/sneaker_watcher_bot/services/scrapers/atmos/detect_collections_change.rb
@@ -3,30 +3,44 @@ module Service
     module Atmos
       class DetectCollectionsChange < Base
         def self.interval_seconds
-          15
+          10
         end
 
         def perform
           response = RestClient.get("#{base_url}/collections")
           html = Nokogiri::HTML(response.body)
           html.xpath("//*[@id='panel']//div//a//img").each do |img|
-            # check if sold
-            product_sold_out = img.parent.parent.xpath(".//*[contains(text(),'sold')]")
-            product_url = img.parent.attributes['href'].value
-            product_name = img.attributes['alt'].value
-            product_img = img.attributes['src'].value
-            product_img = "https:#{product_img}"
-            product_hash = {
-              name: product_name,
-              url: product_url,
-              image: product_img
-            }
-            next if product_sold_out.present? || # already sold out
-              raffle?(product_url) || # is a raffle
-              !relevant_product?(product_name) || # not relevant
-              !new_product?(product_hash) # not a new product
+            begin
+              product_sold_out = img.parent.parent.xpath(".//*[contains(text(),'sold')]")
+              product_url = img.parent.attributes['href'].value
+              product_name = img.attributes['alt'].value
+              product_img = img.attributes['src'].value
+              product_img = "https:#{product_img}"
+              product_hash = {
+                name: product_name,
+                url: product_url,
+                image: product_img
+              }
+              next if raffle?(product_url) || # is a raffle
+                product_sold_out.present? || # already sold out
+                !relevant_product?(product_name) # not relevant
+
+              product_detail = get_product_detail(product_url)
+              product_hash.merge!(product_detail)
+
+              next if raffle?(product_hash[:tags]) || # is a raffle
+                !new_product?(product_hash) # not a new product
 
               send_message(product_hash)
+            rescue => e
+              log_object = {
+                tags: self.class.name.underscore,
+                message: e.message,
+                backtrace: e.backtrace.take(5),
+                instagram_username: @username
+              }
+              SneakerWatcherBot.logger.error(log_object)
+            end
           end
         end
 
@@ -37,7 +51,7 @@ module Service
         end
 
         def redis_key(identifier)
-          "atmos_latest_collections_#{identifier.downcase}"
+          "atmos_latest_collections_#{identifier}"
         end
 
         def required_attributes?(product_hash)
@@ -51,7 +65,7 @@ module Service
         end
 
         def new_product?(product_hash)
-          key = redis_key(product_hash[:name])
+          key = redis_key(product_hash[:slug])
           product_cache = SneakerWatcherBot.redis.get(key)
           return true if product_cache.blank?
           SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
@@ -66,8 +80,32 @@ module Service
           message = "<strong>ATMOS COLLECTIONS UPDATE DETECTED!</strong>\n"\
             "#{product_hash[:name]}\n"\
             "<a href='#{base_url}#{product_hash[:url]}'>CHECK IT OUT!</a>"
+
+          message += "\n\n<strong>AVAILABLE SIZE</strong>:\n" if product_hash[:sizes].present?
+          product_hash[:sizes].each do |size|
+            checkout_link = "#{base_url}/cart/#{size[:id]}:1"
+            message += "<strong>#{size[:title]}</strong> "\
+              "<a href='#{checkout_link}'>CHECKOUT</a>\n"
+          end
           TelegramBot.new.send_telegram_photo(message, product_hash[:image])
-          SneakerWatcherBot.redis.set(redis_key(product_hash[:name]), product_hash.to_json)
+          SneakerWatcherBot.redis.set(redis_key(product_hash[:slug]), product_hash.to_json)
+        end
+
+        def get_product_detail(product_url)
+          response = RestClient.get("#{base_url}#{product_url}.json")
+          raw_product_data = JSON.parse(response.body).deep_symbolize_keys
+          sizes = []
+          raw_product_data.dig(:product, :variants).each do |variant|
+            sizes << {
+              id: variant[:id],
+              title: variant[:option1]
+            }
+          end
+          {
+            slug: raw_product_data.dig(:product, :handle),
+            tags: raw_product_data.dig(:product, :tags),
+            sizes: sizes
+          }
         end
       end
     end
