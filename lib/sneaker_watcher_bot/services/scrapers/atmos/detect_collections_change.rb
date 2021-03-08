0d5993e8c4aafa08233598c00eed4c54eeaf98e9
Windu Risky Adiatma <30619149+windurisky@users.noreply.github.com>
2021-03-08 10:53:47 +0700

    Atmos restock detection (#37)
    * now detect restock and delete cache on sold out
    
    * optimization
    
    * make it 4 seconds
    
    * remove pry
    
    Co-authored-by: windurisky <windu.risky@bukalapak.com>

--- lib/sneaker_watcher_bot/services/scrapers/atmos/detect_collections_change.rb
+++ lib/sneaker_watcher_bot/services/scrapers/atmos/detect_collections_change.rb
@@ -3,33 +3,55 @@ module Service
     module Atmos
       class DetectCollectionsChange < Base
         def self.interval_seconds
-          5
+          4
         end
 
         def perform
-          response = RestClient.get("#{base_url}/collections")
-          html = Nokogiri::HTML(response.body)
-          html.xpath("//*[@id='panel']//div//a//img").each do |img|
+          collections = ENV['ATMOS_COLLECTIONS'].split(',').map(&:strip).compact
+
+          collections.each do |collection|
+            scrape_collection_products(collection)
+          end
+        end
+
+        private
+
+        def scrape_collection_products(collection)
+          response = RestClient.get("#{base_url}/collections/#{collection}/products.json")
+          raw_product_data = JSON.parse(response.body).deep_symbolize_keys
+          raw_product_data.dig(:products).take(8).each do |product|
             begin
-              product_sold_out = img.parent.parent.xpath(".//*[contains(text(),'sold')]")
-              product_url = img.parent.attributes['href'].value
-              product_name = img.attributes['alt'].value
-              product_img = img.attributes['src'].value
-              product_img = "https:#{product_img}"
+              product_available = product[:variants].any? {|v| v[:available] == true }
+              product_slug = product[:handle]
+
+              if !product_available
+                delete_cache(product_slug)
+                next
+              end
+
+              product_name = product[:title]
+              next if !relevant_product?(product_name) # not relevant
+
+              product_url = "/collections/#{collection}/products/#{product[:handle]}"
+              product_img = product[:images]&.first&.dig(:src)
               product_hash = {
+                slug: product_slug,
                 name: product_name,
                 url: product_url,
                 image: product_img
               }
-              next if raffle?(product_url) || # is a raffle
-                product_sold_out.present? || # already sold out
-                !relevant_product?(product_name) # not relevant
 
-              product_detail = get_product_detail(product_url)
-              product_hash.merge!(product_detail)
+              product_hash[:sizes] = []
+              product[:variants].each do |variant|
+                next if !variant[:available]
+                product_hash[:sizes] << {
+                  id: variant[:id],
+                  title: variant[:option1]
+                }
+              end
 
-              next if raffle?(product_hash[:tags]) || # is a raffle
-                !new_product?(product_hash) # not a new product
+              next if raffle?(product[:tags].join(',')) || # is a raffle
+                !need_notify?(product_hash) # if new product / new stock
 
               send_message(product_hash)
             rescue => e
@@ -44,8 +66,6 @@ module Service
           end
         end
 
-        private
-
         def base_url
           ENV['ATMOS_BASE_URL']
         end
@@ -54,22 +74,30 @@ module Service
           "atmos_latest_collections_#{identifier}"
         end
 
-        def required_attributes?(product_hash)
-          # invalid if any of these attribute is blank
-          product_hash[:name].blank? || product_hash[:url].blank? || product_hash[:image].blank?
-        end
-
         def whitelisted_products
           @whitelisted_products ||= ENV['ATMOS_COLLECTIONS_WHITELISTED_PRODUCTS'].split(",")
             .map(&:strip).map(&:downcase).compact
         end
 
-        def new_product?(product_hash)
+        def need_notify?(product_hash)
+          # check if there's any existing cache yet
           key = redis_key(product_hash[:slug])
           product_cache = SneakerWatcherBot.redis.get(key)
           return true if product_cache.blank?
-          SneakerWatcherBot.redis.expireat(key, redis_expiry.to_i)
+
+          # if old cache exists,
+          # determine whether new cache has any new available sizes (new stock)
+          old_hash = JSON.parse(product_cache).deep_symbolize_keys
+          new_stock = (product_hash[:sizes] - old_hash[:sizes]).any?
+          return true if new_stock
+
+          # save latest cache to accommodate stock/data changes
+          SneakerWatcherBot.redis.setex(key, redis_expiry.to_i, product_hash.to_json)
           return false
+        end
+
+        def delete_cache(identifier)
+          SneakerWatcherBot.redis.del(redis_key(identifier))
         end
 
         def redis_expiry
@@ -88,24 +116,7 @@ module Service
               "<a href='#{checkout_link}'>CHECKOUT</a>\n"
           end
           TelegramBot.new.send_telegram_photo(message, product_hash[:image])
-          SneakerWatcherBot.redis.set(redis_key(product_hash[:slug]), product_hash.to_json)
-        end
-
-        def get_product_detail(product_url)
-          response = RestClient.get("#{base_url}#{product_url}.json")
-          raw_product_data = JSON.parse(response.body).deep_symbolize_keys
-          sizes = []
-          raw_product_data.dig(:product, :variants).each do |variant|
-            sizes << {
-              id: variant[:id],
-              title: variant[:option1]
-            }
-          end
-          {
-            slug: raw_product_data.dig(:product, :handle),
-            tags: raw_product_data.dig(:product, :tags),
-            sizes: sizes
-          }
+          SneakerWatcherBot.redis.setex(redis_key(product_hash[:slug]), redis_expiry.to_i, product_hash.to_json)
         end
       end
     end
