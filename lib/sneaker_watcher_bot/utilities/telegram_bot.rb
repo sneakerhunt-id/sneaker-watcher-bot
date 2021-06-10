781d8c79861160e5abf8209b6e65156da9494113
windurisky <windu.risky@bukalapak.com>
2021-06-10 20:40:02 +0700

    flag to easily disable telegram message with one env change

--- lib/sneaker_watcher_bot/utilities/telegram_bot.rb
+++ lib/sneaker_watcher_bot/utilities/telegram_bot.rb
@@ -5,6 +5,7 @@ class TelegramBot
   end
 
   def send_telegram_message(text, parse_mode = 'HTML')
+    return if ENV['DISABLE_TELEGRAM_MESSAGE'].to_i == 1
     Telegram::Bot::Client.run(@token) do |bot|
       bot.api.send_message(
         chat_id: @chat_id,
@@ -16,6 +17,7 @@ class TelegramBot
   end
   
   def send_telegram_photo(text, image_url, parse_mode = 'HTML')
+    return if ENV['DISABLE_TELEGRAM_MESSAGE'].to_i == 1
     Telegram::Bot::Client.run(@token) do |bot|
       bot.api.send_photo(
         chat_id: @chat_id,
